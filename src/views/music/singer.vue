f79d8733a7bc526056774ab299ed3c575619925c
chenmingliang <1825720338@qq.com>
2025-06-03 16:56:07 +0800

    feat: 更新代码

--- src/views/music/singer.vue
+++ src/views/music/singer.vue
@@ -1,6 +1,144 @@
 <template>
-  <div>歌手管理</div>
-</template>
-<script setup lang="ts"></script>
+  <div class="table-box">
+    <ProTable
+      ref="proTable"
+      :columns="columns"
+      :request-api="getTableList"
+      :init-param="initParam"
+      :data-callback="dataCallback"
+      @drag-sort="sortTable"
+    >
+      <!-- 表格 header 按钮 -->
+      <template #tableHeader="scope">
+        <el-button type="primary" :icon="CirclePlus" @click="openDrawer('新增')">新增用户</el-button>
+        <el-button type="primary" :icon="Upload" plain @click="batchAdd">批量添加用户</el-button>
+        <el-button type="danger" :icon="Delete" plain :disabled="!scope.isSelected" @click="batchDelete(scope.selectedListIds)">
+          批量删除用户
+        </el-button>
+      </template>
 
-<style scoped lang="scss"></style>
+      <!-- 表格操作 -->
+      <template #operation="scope">
+        <el-button type="primary" link :icon="View" @click="openDrawer('查看', scope.row)">查看</el-button>
+        <el-button type="primary" link :icon="EditPen" @click="openDrawer('编辑', scope.row)">编辑</el-button>
+        <el-button type="primary" link :icon="Delete" @click="deleteAccount(scope.row)">删除</el-button>
+      </template>
+    </ProTable>
+    <UserDrawer ref="drawerRef" />
+    <ImportExcel ref="dialogRef" />
+  </div>
+</template>
+
+<script setup lang="tsx" name="useProTable">
+import { ref, reactive } from "vue";
+import { User } from "@/api/interface";
+import { useHandleData } from "@/hooks/useHandleData";
+import { ElMessage } from "element-plus";
+import ProTable from "@/components/ProTable/index.vue";
+import ImportExcel from "@/components/ImportExcel/index.vue";
+import UserDrawer from "@/views/proTable/components/UserDrawer.vue";
+import { ProTableInstance, ColumnProps } from "@/components/ProTable/interface";
+import { CirclePlus, Delete, EditPen, Upload, View } from "@element-plus/icons-vue";
+import { getUserList, deleteUser, editUser, addUser, exportUserInfo, BatchAddUser, getUserGender } from "@/api/modules/user";
+
+// ProTable 实例
+const proTable = ref<ProTableInstance>();
+
+// 如果表格需要初始化请求参数，直接定义传给 ProTable (之后每次请求都会自动带上该参数，此参数更改之后也会一直带上，改变此参数会自动刷新表格数据)
+const initParam = reactive({ type: 1 });
+
+// dataCallback 是对于返回的表格数据做处理，如果你后台返回的数据不是 list && total 这些字段，可以在这里进行处理成这些字段
+// 或者直接去 hooks/useTable.ts 文件中把字段改为你后端对应的就行
+const dataCallback = (data: any) => {
+  return {
+    list: data.list,
+    total: data.total
+  };
+};
+
+// 如果你想在请求之前对当前请求参数做一些操作，可以自定义如下函数：params 为当前所有的请求参数（包括分页），最后返回请求列表接口
+// 默认不做操作就直接在 ProTable 组件上绑定	:requestApi="getUserList"
+const getTableList = (params: any) => {
+  let newParams = JSON.parse(JSON.stringify(params));
+  newParams.createTime && (newParams.startTime = newParams.createTime[0]);
+  newParams.createTime && (newParams.endTime = newParams.createTime[1]);
+  delete newParams.createTime;
+  return getUserList(newParams);
+};
+
+// 表格配置项
+const columns = reactive<ColumnProps<User.ResUserList>[]>([
+  { type: "selection", fixed: "left", width: 70 },
+  {
+    prop: "username",
+    label: "姓名",
+    search: { el: "input", tooltip: "我是搜索提示" }
+  },
+  {
+    prop: "gender",
+    label: "简介",
+    // 字典数据（本地数据）
+    // enum: genderType,
+    // 字典请求不带参数
+    enum: getUserGender,
+    // 字典请求携带参数
+    // enum: () => getUserGender({ id: 1 }),
+    search: { el: "select", props: { filterable: true } },
+    fieldNames: { label: "genderLabel", value: "genderValue" }
+  },
+  {
+    prop: "type",
+    label: "类型"
+  },
+  {
+    prop: "createTime",
+    label: "创建时间"
+  },
+  { prop: "operation", label: "操作", fixed: "right", width: 330 }
+]);
+
+// 表格拖拽排序
+const sortTable = ({ newIndex, oldIndex }: { newIndex?: number; oldIndex?: number }) => {
+  console.log(newIndex, oldIndex);
+  console.log(proTable.value?.tableData);
+  ElMessage.success("修改列表排序成功");
+};
+
+// 删除用户信息
+const deleteAccount = async (params: User.ResUserList) => {
+  await useHandleData(deleteUser, { id: [params.id] }, `删除【${params.username}】用户`);
+  proTable.value?.getTableList();
+};
+
+// 批量删除用户信息
+const batchDelete = async (id: string[]) => {
+  await useHandleData(deleteUser, { id }, "删除所选用户信息");
+  proTable.value?.clearSelection();
+  proTable.value?.getTableList();
+};
+
+// 批量添加用户
+const dialogRef = ref<InstanceType<typeof ImportExcel> | null>(null);
+const batchAdd = () => {
+  const params = {
+    title: "用户",
+    tempApi: exportUserInfo,
+    importApi: BatchAddUser,
+    getTableList: proTable.value?.getTableList
+  };
+  dialogRef.value?.acceptParams(params);
+};
+
+// 打开 drawer(新增、查看、编辑)
+const drawerRef = ref<InstanceType<typeof UserDrawer> | null>(null);
+const openDrawer = (title: string, row: Partial<User.ResUserList> = {}) => {
+  const params = {
+    title,
+    isView: title === "查看",
+    row: { ...row },
+    api: title === "新增" ? addUser : title === "编辑" ? editUser : undefined,
+    getTableList: proTable.value?.getTableList
+  };
+  drawerRef.value?.acceptParams(params);
+};
+</script>
